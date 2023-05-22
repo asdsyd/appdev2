ccfaae9be79e4a8d1e4419e741cf58d2db4e0324
Asad Sayeed <ashasd97@gmail.com>
2023-05-23 03:05:31 +0530

    fixed

--- ticketshow/src/views/AdminDashboard.vue
+++ ticketshow/src/views/AdminDashboard.vue
@@ -1,32 +1,15 @@
 
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">{{$store.state.user.username}} Dashboard</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Summary</a>
-          </li>
-        </ul>
-        <button class="btn btn-outline-success" type="submit">Logout</button>
-      </div>
-    </div>
-  </nav>
-  <div class="m-lg-3 card mb-3 border-primary" style="width: 18rem;">
+  <NavBar></NavBar>
+
+  <div v-if="venues"  v-for="v in venues" class="m-lg-3 card mb-3 border-primary" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">Venue_01</h5>
       <p class="card-text">No shows created</p>
       <div>
         <button class=" m-3 rounded-circle btn btn-primary">+</button>
       </div>
-      <button href="#" class="me-3 btn btn-warning">Edit</button>
+      <button class="btn btn-success">Edit</button>
       <button href="#" class="btn btn-danger">Delete</button>
     </div>
   </div>
@@ -41,6 +24,8 @@
   </div>
 </template>
 <script setup>
+import {ref} from "vue";
+import NavBar from "@/views/NavBar.vue";
 import { useStore } from 'vuex';
 import axios from '../axios';
 </script>
--- ticketshow/src/views/CreateVenue.vue
+++ ticketshow/src/views/CreateVenue.vue
@@ -1,25 +1,6 @@
 
 <template>
-
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Username's Dashboard</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Summary</a>
-          </li>
-        </ul>
-        <button class="btn btn-outline-success" type="submit">Logout</button>
-      </div>
-    </div>
-  </nav>
+<NavBar></NavBar>
   <div>
     <form class="m-3" @submit.prevent="handleclick">
       <!-- 2 column grid layout with text inputs for the first and last names -->
@@ -70,6 +51,7 @@
 
 import {reactive} from "vue";
 import axios from "axios";
+import NavBar from "@/views/NavBar.vue";
 
 const venue = reactive({
   name:'',
