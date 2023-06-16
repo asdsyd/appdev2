86c6fa159bdfdbb67e716a764e57edba8d8e6177
Asad Sayeed <ashasd97@gmail.com>
2023-06-16 16:55:09 +0530

    Changes by arbaz

--- flask/rediscache.py
+++ flask/rediscache.py
@@ -2,9 +2,9 @@ from flask_caching import Cache
 
 config = {
     'CACHE_TYPE': 'redis',
-    'CACHE_REDIS_HOST': '172.20.253.185',
+    'CACHE_REDIS_HOST': 'localhost',
     'CACHE_REDIS_PORT': 6379,
-    'CACHE_REDIS_URL': 'redis://172.20.253.185:6379/0',
+    'CACHE_REDIS_URL': 'redis://localhost:6379/0',
     'CACHE_REDIS_PASSWORD': 'arbaz',
     'CACHE_DEFAULT_TIMEOUT': 300 
 }
