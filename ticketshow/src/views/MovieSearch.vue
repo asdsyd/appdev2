9095027e4a9e08c7def99a11f2ebec564da4955d
Asad Sayeed <ashasd97@gmail.com>
2023-06-16 16:54:58 +0530

    fixed

---
+++ ticketshow/src/views/MovieSearch.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import NavBar from "@/views/NavBar.vue";
+import UserBottomNavBar from "@/views/UserBottomNavBar.vue";
+</script>
+
+<template>
+  <nav-bar/>
+
+  <user-bottom-nav-bar/>
+
+</template>
+
+<style scoped>
+
+</style>
